f5967d1f1c7d9d4794b89101baddc409e88c100b
Labbi Ahmed <30775026+labbiahmed@users.noreply.github.com>
2017-09-25 00:39:18 +0600

    Insert some lowercase input in this co

--- AllTest/src/Gene_Find/FinedGenePart2.java
+++ AllTest/src/Gene_Find/FinedGenePart2.java
@@ -2,73 +2,58 @@ package Gene_Find;
 import edu.duke.*;
 import java.util.*;
 public class FinedGenePart2 {
-	public String findSimpleGene(String dna){
-        int startIndex = dna.indexOf("ATG");
+	
+	public String findSimpleGene(String dna , String startCodon , String stopCodon){
+		String result = "";
+		boolean flag = false; 
+		if((int)dna.charAt(0) >= 97){
+			dna = dna.toUpperCase();
+			flag = true;
+			
+		}
+        int startIndex = dna.indexOf(startCodon);
         if(startIndex == -1){
             return "";
         }
-        int stopIndex = dna.indexOf("TAA");
+        int stopIndex = dna.indexOf(stopCodon);
         if(stopIndex == -1){
             return "";
         }
         if((stopIndex - startIndex + 3) % 3 == 0){
-            return dna.substring(startIndex , stopIndex+3);
+            result = dna.substring(startIndex , stopIndex+3);
         }
-        return "";
+        if(flag)
+        	result = result.toLowerCase();
+        
+        return result;
     }
     
     public void testSimpleGene(){
         String DNA , sub_string; 
         DNA = "TAGCTATTCTGCCGAATC"; // gene sequence without ATG and TAA
-        System.out.println("DNA strand is " + DNA);
-        sub_string = findSimpleGene(DNA);
-        if(sub_string.length() != 0){
-            System.out.println("Gene is : " + sub_string);
-        }
-        else{
-            System.out.println("Gene is :");
-        }
+        sub_string = findSimpleGene(DNA , "ATG" , "TAA");
+        System.out.println("DNA is: " + DNA  + "\n" + "Gene is: " + sub_string);
         
         DNA = "TCGTAGGCATAATGCCGA"; // gene sequence without ATG
-        System.out.println("DNA strand is " + DNA);
-        sub_string = findSimpleGene(DNA);
-        if(sub_string.length() != 0){
-            System.out.println("Gene is : " + sub_string);
-        }
-        else{
-            System.out.println("Gene is :");
-        }
+        sub_string = findSimpleGene(DNA , "ATG" , "TAA");
+        System.out.println("DNA is: " + DNA  + "\n" + "Gene is: " + sub_string);
         
         DNA = "TACATGTCCAGGTGGGTCTGC";                         // gene sequence with ATG and without TAA
-        System.out.println("DNA strand is " + DNA);
-        sub_string = findSimpleGene(DNA);
-        if(sub_string.length() != 0){
-            System.out.println("Gene is : " + sub_string);
-        }
-        else{
-            System.out.println("Gene is :");
-        }
+        sub_string = findSimpleGene(DNA , "ATG" , "TAA");
+        System.out.println("DNA is: " + DNA  + "\n" + "Gene is: " + sub_string);
         
         DNA = "TACATGTCCAGGTGGGTCTGCTAA";                     /// gene sequence with ATG , TAA and sub_string is multiple of 3   
-        System.out.println("DNA strand is " + DNA);
-        sub_string = findSimpleGene(DNA);
-        if(sub_string.length() != 0){
-             System.out.println("Gene is : " + sub_string);
-        }
-        else{
-            System.out.println("Gene is :");
-        }
+        sub_string = findSimpleGene(DNA , "ATG" , "TAA");
+        System.out.println("DNA is: " + DNA  + "\n" + "Gene is: " + sub_string);
         
         DNA = "TACATGTCCAGGTGGGTCTTGCTAA";                 // gene sequence sub_string is not multiple of 3
-        System.out.println("DNA strand is " + DNA);
-        sub_string = findSimpleGene(DNA);
-        if(sub_string.length() != 0){
-             System.out.println("Gene is : " + sub_string);
-        }
-        else{
-            System.out.println("Gene is :");
-        }
-   
+        sub_string = findSimpleGene(DNA , "ATG" , "TAA");
+        System.out.println("DNA is: " + DNA  + "\n" + "Gene is: " + sub_string);
+        
+        DNA = "atgcttcattaa";
+        sub_string = findSimpleGene(DNA , "ATG" , "TAA");
+        System.out.println("DNA is: " + DNA  + "\n" + "Gene is: " + sub_string);
+        
     }
     public static void main(String arg[]){
     	FinedGenePart2 ob = new FinedGenePart2();

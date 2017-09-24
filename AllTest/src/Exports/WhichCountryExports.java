6c01e8bed37b4ff585b575fb9fd23f42bff55ac3
Labbi Ahmed <30775026+labbiahmed@users.noreply.github.com>
2017-09-25 02:00:48 +0600

    done all works

--- AllTest/src/Exports/WhichCountryExports.java
+++ AllTest/src/Exports/WhichCountryExports.java
@@ -1,5 +1,27 @@
 package Exports;
+import java.util.*;
 
+import edu.duke.*;
+import org.apache.commons.csv.*;
 public class WhichCountryExports {
-
+	public void listExports(CSVParser parser, String exportOfInterst) {
+        for(CSVRecord record : parser){
+            String export = record.get("Exports");
+            if(export.contains(exportOfInterst)) {
+                String country = record.get("Country");
+                System.out.println(country);
+            }
+        }
+    }
+    public void whoExportCoffee() {
+        FileResource fr = new FileResource();
+        CSVParser parser = fr.getCSVParser();
+        listExports(parser , "coffee");
+    }
+    
+    public static void main(String[] args){
+    	
+    	WhichCountryExports object = new WhichCountryExports();
+    	object.whoExportCoffee();
+    }
 }
